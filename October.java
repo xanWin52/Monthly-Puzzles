21e00e7da25e32b01bcfa2db3383bf621e81d740
Xander Nguyen <77035811+xanWin52@users.noreply.github.com>
2023-10-30 12:53:17 -0500

    Create October.java

---
+++ October.java
@@ -0,0 +1,17 @@
+public class October{
+    public static void main(String[] args) {
+        int res = 1;
+        for(int i = 3558; i <= 10000000; i ++){
+            if(isSpooky(i)){
+
+            }
+        }
+    }
+
+    public static boolean isSpooky(int x){
+        int temp = 0;
+        while(x > 0){
+            
+        }
+    }
+}
