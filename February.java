dc50453005bf528378d6413acb96d784e1dd066f
Xander Nguyen <77035811+xanWin52@users.noreply.github.com>
2024-02-06 12:52:32 -0600

    Create February.java

---
+++ February.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+public class February {
+    public static void main(String[] args) {
+        ArrayList<Integer> longMonths = new ArrayList<Integer>();
+        longMonths.add(1);
+        longMonths.add(3);
+        longMonths.add(5);
+        longMonths.add(7);
+        longMonths.add(8);
+        longMonths.add(10);
+        longMonths.add(12);
+        ArrayList<Integer> shortMonths = new ArrayList<Integer>();
+        shortMonths.add(4);
+        shortMonths.add(6);
+        shortMonths.add(9);
+        shortMonths.add(11);
+        int count = 0;
+        for(int year = 24; year < 25; year ++){
+            for(int month = 1; month <= 12; month ++){
+                int numDays = 0;
+                if(longMonths.contains(month)){ numDays = 31;}
+                else if(shortMonths.contains(month)){ numDays = 30;}
+                else if(month == 2) {numDays = 28;}
+
+                for(int day = 1; day <= numDays; day ++){
+                    int date = month * 10000 + day * 100 + year;
+                    Set<Integer> factors = factor(date);
+                    System.out.println(factors);
+                    int total = 0;
+                    for(int i : factors){
+                        total += i;
+                    }
+                    System.out.println(total);
+                    int target = total - date;
+                    if(factors.contains(target)){
+                        count ++;
+                    }
+                }
+            }
+        }
+        System.out.println(count);
+    }
+
+    public static Set<Integer> factor(int num ){
+        Set<Integer> result = new HashSet<Integer>();
+        result.add(1);
+        for(int i = 2; i < (int)Math.ceil(Math.sqrt(num)); i ++){
+            if(!result.contains(i) && num % i == 0){
+                result.add(i);
+                result.add(num / i);
+            }
+        }
+        return result;
+    }
+}
