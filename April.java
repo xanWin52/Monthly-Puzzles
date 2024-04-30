7f153f453cc7fcf7fc08e21e7ee871e5227e43a1
Xander Nguyen <77035811+xanWin52@users.noreply.github.com>
2024-04-30 16:01:27 -0500

    Create April.java

---
+++ April.java
@@ -0,0 +1,10 @@
+public class April {
+    public static void main(String[] args) {
+        int curOct = 1;
+        int curBoth = 0;
+
+        while(true){
+            int oct = curOct * (3 * curOct - 2);
+        }
+    }    
+}
